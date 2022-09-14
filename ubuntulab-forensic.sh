164b8b1264c78ffc6898b66fdb54e6fa4071e983
1259iknowthat <huaduy1259@gmail.com>
2022-09-14 11:20:11 +0700

    changes

--- ubuntulab-forensic.sh
+++ ubuntulab-forensic.sh
@@ -4,8 +4,7 @@ GREEN='\033[0;32m'
 sudo apt-get update
 printf ${RED}'Install Volatility 2 and 3\n'${GREEN}
 sudo apt-get install -y build-essential git libdistorm3-dev yara libraw1394-11 libcapstone-dev capstone-tool tzdata
-sudo apt-get install -y python2 python2.7-dev 
-sudo apt-get install -y libpython2-dev
+sudo apt-get install -y python2 python2.7-dev libpython2-dev
 curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
 sudo python2 get-pip.py
 sudo python2 -m pip install -U setuptools wheel
@@ -21,7 +20,7 @@ echo 'export PATH=/home/ubuntu/.local/bin:$PATH' >> ~/.bashrc
 sudo apt install -y git 
 git clone https://github.com/superponible/volatility-plugins.git
 sudo cp ~/lab/volatility-plugins/* ~/.local/lib/python2.7/site-packages/volatility/plugins
-read -n 1 -r -s -p "Press any key to continue..." key 
+read -n 1 -r -s -p "Press any key to continue...\n" key 
 printf ${RED}'Install Docker\n'${GREEN}
 sudo apt install gnome-terminal
 sudo apt-get update
@@ -34,14 +33,12 @@ echo \
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 docker pull dominicbreuker/stego-toolkit
-read -n 1 -r -s -p "Press any key to continue..." key
+read -n 1 -r -s -p "Press any key to continue...\n" key
 printf ${RED}'Install Metasploit\n'${GREEN}
 curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && chmod 755 msfinstall && ./msfinstall
-read -n 1 -r -s -p "Press any key to continue..." key
 printf ${RED}'Install Autopsy\n'${GREEN}
 sudo apt install -y autopsy
-read -n 1 -r -s -p "Press any key to continue..." key
+read -n 1 -r -s -p "Press any key to continue...\n" key
 sudo apt update
-sudo apt install -y neofetch lolcat htop 
-sudo apt install -y bpytop
+sudo apt install -y neofetch lolcat htop bpytop
 sudo apt upgrade 
