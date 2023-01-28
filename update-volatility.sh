75d4cb4191cade367d4c9bd2705dc7198086665a
1259iknowthat <huaduy1259@gmail.com>
2023-01-28 16:41:19 +0700

    move to volatility-tools repo

--- update-volatility.sh
+++
@@ -1,7 +0,0 @@
-sudo rm -rf ~/.local/lib/python2.7/dist-packages/volatility
-sudo rm `which vol.py`
-sudo rm -rf ~/.local/contrib/plugins
-sudo rm 'which vol'
-python2 -m pip install -U git+https://github.com/volatilityfoundation/volatility.git
-python3 -m pip install -U git+https://github.com/volatilityfoundation/volatility3.git
-export PATH=/home/$USER/.local/bin:$PATH
