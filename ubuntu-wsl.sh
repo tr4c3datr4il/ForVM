10c733fa7cea9193f63c52b15fe75b0d82206dba
1259iknowthat <huaduy1259@gmail.com>
2023-02-28 23:20:47 +0700

    remove docker and add some tools

--- ubuntu-wsl.sh
+++ ubuntu-wsl.sh
@@ -1,99 +1,81 @@
 #!/bin/bash
 RED='\033[0;31m'
-GREEN='\033[0;32m'
-sudo apt-get update && sudo apt upgrade -y
+CYAN="\033[0;36m"
+sudo apt update && sudo apt upgrade -y
 
 mkdir ~/lab
 cd ~/lab
 
-printf ${RED}'Installing Volatility 2 and 3\n'${GREEN}
-sudo apt install curl -y
-sudo apt-get install -y build-essential git libdistorm3-dev yara libraw1394-11 libcapstone-dev capstone-tool tzdata
-sudo apt-get install -y python2 python2.7-dev libpython2-dev
-curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
-sudo python2 get-pip.py
-sudo python2 -m pip install -U setuptools wheel
-python2 -m pip install -U distorm3 yara pycrypto pillow openpyxl ujson pytz ipython capstone
-sudo python2 -m pip install yara
-sudo ln -s /usr/local/lib/python2.7/dist-packages/usr/lib/libyara.so /usr/lib/libyara.so
-python2 -m pip install -U git+https://github.com/volatilityfoundation/volatility.git
-sudo apt install -y python3 python3-dev libpython3-dev python3-pip python3-setuptools 
-sudo apt install -y python3-wheel
-python3 -m pip install -U distorm3 pillow openpyxl ujson pytz ipython capstone pefile yara-python pycryptodome jsonschema leechcorepyc python-snappy
-python3 -m pip install -U git+https://github.com/volatilityfoundation/volatility3.git
-export PATH=/home/$USER/.local/bin:$PATH
-sudo apt install -y git 
-git clone https://github.com/superponible/volatility-plugins.git
-sudo cp ~/lab/volatility-plugins/* ~/.local/lib/python2.7/site-packages/volatility/plugins/
+echo -e ${RED}'Installing Volatility 2 and 3\n'${CYAN}
+    sudo apt install -y curl build-essential git libdistorm3-dev yara libraw1394-11 libcapstone-dev capstone-tool tzdata
+    sudo apt install -y python2 python2.7-dev libpython2-dev
+    curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
+    sudo python2 get-pip.py
+    sudo python2 -m pip install -U setuptools wheel
+    python2 -m pip install -U distorm3 yara pycryptodome pillow openpyxl ujson pytz ipython capstone construct==2.5.5-reupload
+    sudo ln -s /usr/local/lib/python2.7/dist-packages/usr/lib/libyara.so /usr/lib/libyara.so
+    python2 -m pip install -U git+https://github.com/volatilityfoundation/volatility.git
 
-printf ${RED}'Press ENTER to continue\n'${GREEN}
+    sudo apt install -y python3 python3-dev libpython3-dev python3-pip python3-setuptools python3-wheel
+    python3 -m pip install -U distorm3 pillow openpyxl ujson pytz ipython capstone pefile yara-python pycryptodome jsonschema leechcorepyc python-snappy
+    python3 -m pip install -U git+https://github.com/volatilityfoundation/volatility3.git
+    echo -e "export PATH=/home/$USER/.local/bin:$PATH" >> ~/.bashrc
+    git clone https://github.com/superponible/volatility-plugins.git
+    sudo cp ~/lab/volatility-plugins/* ~/.local/lib/python2.7/site-packages/volatility/plugins/
+    git clone https://github.com/volatilityfoundation/volatility.git
+
+echo -e ${RED}'Press ENTER to continue\n'${CYAN}
 read a
 
-### I'm trying to fix Docker issue on Windows. Maybe there's a way to link Docker with WSL terminal instead of using Powershell
-# printf ${RED}'Installing Docker\n'${GREEN}
-# sudo apt install gnome-terminal -y
-# sudo apt-get update
-# sudo apt-get install ca-certificates curl gnupg lsb-release -y
-# sudo mkdir -p /etc/apt/keyrings
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-# echo \
-#   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-#   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-# sudo apt-get update -y
-# sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
-# sudo docker pull dominicbreuker/stego-toolkit
+echo -e ${RED}'Install tshark and Fakenet\n'${CYAN}
+    sudo apt install tshark -y
+    wget https://github.com/mandiant/flare-fakenet-ng/releases/download/v1.4.11/fakenet1.4.11.zip
 
-printf ${RED}'Press ENTER to continue\n'${GREEN}
+echo -e ${RED}'Install John the Ripper & Hashcat & Wordlists\n'${CYAN}
+    sudo apt install hashcat snapd -y
+    sudo snap install john-the-ripper
+    git clone https://github.com/danielmiessler/SecLists.git
+    git clone https://github.com/3ndG4me/KaliLists.git
+    cd KaliLists/ 
+    gunzip rockyou.txt.gz 
+    cd ~ 
+    echo "alias 'wordlists'='echo ~/lab/KaliList ~/lab/SecLists'" >> ~/.zshrc
+
+echo -e ${RED}'Install Stego and OSINT tools\n'${CYAN}
+    cd ~/lab
+    sudo apt install exiftool steghide -y
+    sudo gem install zsteg
+    wget https://github.com/RickdeJager/stegseek/releases/download/v0.6/stegseek_0.6-1.deb
+    chmod 777 ./stegseek_0.6-1.deb
+    sudo apt install ./stegseek_0.6-1.deb -y
+    sudo apt install default-jre
+    wget http://www.caesum.com/handbook/Stegsolve.jar -O stegsolve.jar
+    chmod +x stegsolve.jar
+    git clone https://github.com/p1ngul1n0/blackbird
+    cd blackbird
+    pip3 install -r requirements.txt
+    pip3 install pipx
+    pipx ensurepath
+    sudo apt install python3.10-venv -y
+    pipx install ghunt
+    sed -i '1i#!/usr/bin/python3' ~/lab/blackbird/blackbird.py
+    sudo cp ~/lab/blackbird/blackbird.py /usr/bin/blackbird.py
+
+echo -e ${RED}'Press ENTER to continue\n'${CYAN}
 read a
 
-printf ${RED}'Install Autopsy\n'${GREEN}
-sudo apt install -y autopsy
+echo -e ${RED}'Install oletools\n'${CYAN}
+    sudo -H pip3 install -U oletools[full]
+    cd ~/lab
+    git clone https://github.com/jesparza/peepdf.git
+    cd peepdf
+    sed -i '1i#!/usr/bin/python2.7' peepdf.py
+    sudo cp -r * /usr/bin/
+    cd
 
-printf ${RED}'Install Wireshark\n'${GREEN}
-sudo apt install wireshark -y
-
-printf ${RED}'Please make sure you choose "YES" while installing Wireshark\n'${GREEN}
+echo -e ${RED}'Press ENTER to continue\n'${CYAN}
 read a
-sudo dpkg-reconfigure wireshark-common
-sudo usermod -a -G wireshark ubuntu
-sudo apt install tshark -y
+    sudo apt update -y
+    sudo apt install -y neofetch lolcat batcat nala htop bpytop bison flex dwarfdump openssh-server net-tools openvpn dos2unix
+    sudo apt upgrade -y 
 
-printf ${RED}'Install John the Ripper & Hashcat & Wordlists\n'${GREEN}
-sudo apt-get install hashcat -y
-sudo apt install john -y
-git clone https://github.com/danielmiessler/SecLists.git
-git clone https://github.com/3ndG4me/KaliLists.git
-cd KaliLists/ 
-gunzip rockyou.txt.gz 
-cd ~ 
-
-printf ${RED}'Install Stego and OSINT tools\n'${GREEN}
-read a
-cd ~/lab
-sudo apt install exiftool -y
-sudo gem install zsteg
-wget https://github.com/RickdeJager/stegseek/releases/download/v0.6/stegseek_0.6-1.deb
-chmod 777 ./stegseek_0.6-1.deb
-sudo apt install ./stegseek_0.6-1.deb -y
-sudo apt install default-jre
-wget http://www.caesum.com/handbook/Stegsolve.jar -O stegsolve.jar
-chmod +x stegsolve.jar
-git clone https://github.com/p1ngul1n0/blackbird
-cd blackbird
-pip install -r requirements.txt
-pip3 install pipx
-pipx ensurepath
-sudo apt install python3.10-venv -y
-pipx install ghunt
-printf ${RED}'Press ENTER to continue\n'${GREEN}
-read a
-
-printf ${RED}'Install oletools\n'${GREEN}
-sudo -H pip3 install -U oletools[full]
-
-printf ${RED}'Press ENTER to continue\n'${GREEN}
-read a
-sudo apt update -y
-sudo apt install -y neofetch lolcat htop bpytop bison flex dwarfdump
-sudo apt upgrade -y 
-printf ${RED}'Please restart your terminal.\n'${GREEN}
